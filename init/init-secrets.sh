c825800526113ca1787bc7a0cf2fff714e4da379
lgdd <louis-guillaume.durand@liferay.com>
2022-09-21 08:49:57 +0200

    chore: move init secrets into json file

--- init/init-secrets.sh
+++ init/init-secrets.sh
@@ -4,5 +4,5 @@ curl \
     --header "X-Vault-Token: ${VAULT_TOKEN}" \
     --header "Content-Type: application/json" \
     --request POST \
-    --data '{"data": {"LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_USERNAME": "ray", "LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_PASSWORD": "super-secret-password"}}' \
+    --data @secrets.json \
     ${VAULT_ADDR}/v1/secret/data/liferay
